22752d5b11e917c9807e5f8c8f486936c5fed2f1
Linnea <150425979+linwox@users.noreply.github.com>
2024-02-13 16:15:30 +0100

    Adding some more tests for Product, error handling and javadoc

--- src/test/java/se/yrgo/ProductTest.java
+++ src/test/java/se/yrgo/ProductTest.java
@@ -3,8 +3,38 @@ package se.yrgo;
 import org.junit.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for the Product class
+ */
 public class ProductTest {
 
+    /**
+     * Test for constructor with invalid ID
+     */
+    @Test
+    public void testInvalidProductId() {
+        assertThrows(IllegalArgumentException.class, () -> new Product(111, "McFlurry vanilla", 15.00));
+    }
+
+    /**
+     * Test for constructor with invalid name
+     */
+    @Test
+    public void testInvalidName() {
+        assertThrows(NullPointerException.class, () -> new Product(1101, null, -15.00));
+    }
+
+    /**
+     * Test for constructor with invalid price
+     */
+    @Test
+    public void testInvalidPrice() {
+        assertThrows(IllegalArgumentException.class, () -> new Product(1101, "McFlurry vanilla", -15.00));
+    }
+
+    /**
+     * Test for calculatePricePerProduct() method 
+     */
     @Test
     public void testCalculatePricePerProduct() {
         Product product = new Product(1101, "McFlurry vanilla", 15.00);
